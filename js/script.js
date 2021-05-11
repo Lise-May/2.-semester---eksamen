629fb434842f4b2b62aec6aba1499970c9886753
Lise-May <71006816+Lise-May@users.noreply.github.com>
2021-05-11 14:46:10 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+/* ---------- hamburger menu --------- */
+function onClickMenu() {
+    document.getElementById("menu").classList.toggle("change");
+    
+    document.querySelector(".nav_list").classList.toggle("change");
+}
