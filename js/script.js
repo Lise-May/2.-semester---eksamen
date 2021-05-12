ed9f76e3becdc750f0b32062a0484a262dc9ce0f
jrKruger89 <35734267+jrKruger89@users.noreply.github.com>
2021-05-12 16:39:06 +0200

    Header & footer done?

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
+$(document).ready(function () {
+    $(".open_menu").click(function () {
+        $(".sub_nav").slideToggle();
+    });
+});
 /* ---------- hamburger menu --------- */
 function onClickMenu() {
     document.getElementById("menu").classList.toggle("change");
-    
+
     document.querySelector(".nav_list").classList.toggle("change");
 }
