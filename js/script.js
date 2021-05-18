9f0b6f1bcdb68d11301bfb8c5278fe7d7a948f24
jrKruger89 <35734267+jrKruger89@users.noreply.github.com>
2021-05-18 15:32:28 +0200

    Index page hover effects for pc

--- js/script.js
+++ js/script.js
@@ -2,7 +2,52 @@ $(document).ready(function () {
     $(".open_menu").click(function () {
         $(".sub_nav").slideToggle();
     });
+
+    // Video background
+    $("[data-youtube]").youtube_background();
+
+    if ($(window).width() > 950) {
+        $(".product_article").each(function () {
+            $(this).mouseenter(function () {
+                $(".link_button", this).animate({
+                    backgroundColor: "#af0404",
+                    fontSize: "20px",
+                });
+                $(".index_product_img", this).animate({
+                    height: "+=50px",
+                    width: "+=50px",
+                });
+            });
+            $(this).mouseleave(function () {
+                $(".link_button", this).animate({
+                    backgroundColor: "transparent",
+                    fontSize: "0px",
+                });
+                $(".index_product_img", this).animate({
+                    height: "-=50px",
+                    width: "-=50px",
+                });
+            });
+        });
+        $(".index_guidance_article").each(function () {
+            $(this).mouseenter(function () {
+                $(".index_guidance_button", this).animate({
+                    backgroundColor: "#af0404",
+                    fontSize: "20px",
+                });
+                $(".index_guidance_img", this).addClass("grayscale");
+            });
+            $(this).mouseleave(function () {
+                $(".index_guidance_button", this).animate({
+                    backgroundColor: "transparent",
+                    fontSize: "0px",
+                });
+                $(".index_guidance_img", this).removeClass("grayscale");
+            });
+        });
+    }
 });
+
 /* ---------- hamburger menu --------- */
 function onClickMenu() {
     document.getElementById("menu").classList.toggle("change");
